a10bd9b97d0aa2562ba48f5057bf80af555b78be
mihnea.tudor <mihnea.tudor01@gmail.com>
2022-04-02 16:33:17 +0300

    Initial commit

---
+++ src/main/java/com/mihneatudor/softbinatorcharityproject/SoftbinatorCharityProjectApplication.java
@@ -0,0 +1,13 @@
+package com.mihneatudor.softbinatorcharityproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoftbinatorCharityProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SoftbinatorCharityProjectApplication.class, args);
+    }
+
+}
